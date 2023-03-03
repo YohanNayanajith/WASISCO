aad8771b2a393a46af734f4bc501acde54c4dffa
YohanNayanajith <yohannayanajith40@gmail.com>
2023-03-03 23:25:03 +0530

    Second Section finish

--- src/components/secondSection/SecondSection.js
+++ src/components/secondSection/SecondSection.js
@@ -6,20 +6,20 @@ const SecondSection = ({ reference }) => {
   return (
     <section ref={reference} className="background-main-color">
       <div className="container second-section-header">
-        <div className="second-section-container-second">
+        <div className="justify-content-center second-section-container-second">
           Every project made by wasisco team is created at the intersection
           between design and technology, making the future — today
         </div>
       </div>
       <div className="container second-section-container">
-        <div className="col-12 col-sm-3">
+        <div className="col-12 col-sm-3 align-items-center second-section-container-image">
           <img
             src="https://res.cloudinary.com/midefulness/image/upload/v1677783910/WASISCO/secondSection/ellipseWithArrow_xhqwkl.png"
             alt="Eclipse"
           />
         </div>
         <div className="col-12 offset-sm-1 col-sm-8">
-          <div className="row second-section-container-content">
+          <div className="row justify-content-center second-section-container-content">
             <div className="row second-section-second-container-second-content">
               With a background in design, We work closely with design focused
               teams to build websites and micro-sites for companies and
