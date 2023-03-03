151ca7471f0c4b98fb6bf9453c1d17a2627f4bdf
YohanNayanajith <yohannayanajith40@gmail.com>
2023-03-04 00:27:09 +0530

    Header left completed

--- src/components/serviceHeader/ServiceHeader.js
+++ src/components/serviceHeader/ServiceHeader.js
@@ -4,17 +4,17 @@ import "./ServiceHeader.css";
 const ServiceHeader = (props) => {
   return (
     <section className="background-main-color">
-      <div className="row service-header-main">
-        <div className="col-1 offset-1 service-header-main-number">
+      
+      <div className="service-header-main">
+      <div className="service-header-offset"></div>
+        <div className="service-header-main-number">
           <img
             src="https://res.cloudinary.com/midefulness/image/upload/v1677867835/WASISCO/serviceHeader/Ellipse_22_1_xc66or.png"
             alt="white-eclipse"
           />
           <div>{props.headerNumber}</div>
-          
         </div>
-        <div className="col-3 service-header-main-content">{props.header}</div>
-        <div className="col-8"></div>
+        <div className="service-header-main-content">{props.header}</div>
       </div>
     </section>
   );
